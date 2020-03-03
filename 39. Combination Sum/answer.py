1cf20d4079acf763f575cb0276cea21c3fe4b468
LennyDuan <hod8@aber.ac.uk>
2020-03-03 20:20:41 +0000

    complete 39

--- 39. Combination Sum/answer.py
+++ 39. Combination Sum/answer.py
@@ -1,8 +1,23 @@
-def combinationSum(candidates: list, target: int) -> list[list[int]]:
+def combinationSum(candidates: list, target: int) -> list:
     results = []
 
+    def recursive(current_list, nums, index, target):
+        if target == 0:
+            results.append(current_list)
+            return
 
+        for i in range(index, len(nums)):
+            if target < 0:
+                break
+            recursive(current_list + [nums[i]], nums, i, target - nums[i])
 
+    recursive([], candidates, 0, target)
+    print(results)
+    return results
+    recursive([], candidates, 0, target)
+
+    print(results)
     return results
 
-combinationSum([2,3,5], 8)
+
+combinationSum([2, 3, 5], 8)
