0aa1489826c098988ce7dc7dc71cbf2521dfe04a
LennyDuan <hod8@aber.ac.uk>
2020-05-01 12:31:34 +0100

    complete anwser 2

--- 78. Subsets/answer2.py
+++ 78. Subsets/answer2.py
@@ -1,5 +1,18 @@
+import copy
+
 def subsets(nums: list()) -> list():
-    results = []
+    results = [[]]
+
+    for num in nums:
+        new_list = []
+        origin_result = copy.deepcopy(results)
+        for curr in origin_result:
+            curr.append(num)
+            new_list.append(curr)
+        results += origin_result
+
+    # for num in nums:
+    #     results += [curr + [num] for curr in results]
 
     print(results)
     return results
