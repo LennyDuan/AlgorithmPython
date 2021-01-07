a6357fc607bd18f1078984035245d2409e1123c0
LennyDuan <hod8@aber.ac.uk>
2021-01-07 11:37:29 +0000

    211

--- 211. Design Add and Search Words Data Structure/answer.py
+++ 211. Design Add and Search Words Data Structure/answer.py
@@ -4,11 +4,33 @@ class WordDictionary:
         """
         Initialize your data structure here.
         """
-        self.dic = ''
+        self.trie = {}
 
     def addWord(self, word: str) -> None:
-        self.dic += word
+        trie = self.trie
+        for c in word:
+            if c not in trie:
+                trie[c] = {}
+            trie = trie[c]
+        trie['#'] = {}
 
     def search(self, word: str) -> bool:
-        s_w = word.replace(',', '')
-        return s_w in self.dic
+        return self.dfs(self.trie, word)
+
+    def dfs(self, trie, word):
+        if not word and '#' in trie:
+            return True
+        elif not word or not trie:
+            return False
+
+        f_c = word[0]
+
+        if f_c == '.':
+            return any(self.dfs(trie[c], word[1:]) for c in trie.keys())
+        else:
+            if f_c in trie:
+                trie = trie[f_c]
+                return self.dfs(trie, word[1:])
+            else:
+                return False
+
